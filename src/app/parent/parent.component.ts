4b1adee20da975358c29cfdd708bf4a3698a787f
Ananthu <ananthuajith6587@gmail.com>
2024-03-21 19:59:21 +0530

    style

--- src/app/parent/parent.component.ts
+++ src/app/parent/parent.component.ts
@@ -12,20 +12,22 @@ export class ParentComponent {
 
   tableData = [
     { name: 'John', age: 30, country: 'USA' },
+    { name: 'Aparna', age: 34, country: 'India' },
     { name: 'Emily', age: 25, country: 'Canada' },
-    { name: 'Daniel', age: 40, country: 'UK' }
+    { name: 'Daniel', age: 40, country: 'UK' },
+    { name: 'Arjun', age: 29, country: 'India' },
+  { name: 'Ajith', age: 42, country: 'India' },
+  { name: 'Alessandro', age: 34, country: 'Italy' }
   ];
 
   constructor() { }
 
   ngOnInit(): void {
     console.log('Parent component initialized');
-    // Any initialization logic can be placed here
   }
 
   ngOnDestroy(): void {
     console.log('Parent component destroyed');
-    // Any cleanup logic can be placed here
   }
 
 }
